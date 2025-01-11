9d9fba902a881d9333b384a5d6b04a1711d02bde
Jakub Żurawski <jakubzuraawski@gmail.com>
2025-01-11 22:00:28 +0100

    secret

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = 'iuafhiufsdghaweioug'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///work_journal.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
